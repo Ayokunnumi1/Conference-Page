2f3bd351e871727af920186c507adbd27497f3c5
Ayokunnumi1 <omololuayk@gmail.com>
2023-07-07 04:48:20 +0100

    Create a new hamburger icon

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener('click', () => (
-  hamburger.classList.toggle('active')
-));
+// const hamburger = document.querySelector('.hamburger');
+// hamburger.addEventListener('click', () => (
+//   hamburger.classList.toggle('active')
+// ));
