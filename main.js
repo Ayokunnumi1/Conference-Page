728ec89dbefb72b04c922c7304746bef0210d72c
Ayokunnumi1 <omololuayk@gmail.com>
2023-07-07 08:38:04 +0100

    Create mobile menu with javascript

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-// const hamburger = document.querySelector('.hamburger');
-// hamburger.addEventListener('click', () => (
-//   hamburger.classList.toggle('active')
-// ));
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const navLinks = document.querySelector('.nav-links');
+const closeIcon = document.querySelector('.close-icon-container');
+
+const menuOpen = () => {
+  navLinks.classList.add('show-link');
+  closeIcon.style.display = 'block';
+  hamburgerIcon.style.display = 'none';
+};
+
+const menuClose = () => {
+  navLinks.classList.remove('show-link');
+  closeIcon.style.display = 'none';
+  hamburgerIcon.style.display = 'block';
+};
+
+hamburgerIcon.addEventListener('click', menuOpen);
+closeIcon.addEventListener('click', menuClose);
